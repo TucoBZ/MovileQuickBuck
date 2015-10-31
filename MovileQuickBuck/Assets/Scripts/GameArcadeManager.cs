906321a1c74ab4c8f094b4db4cc5a064425cbccf
nando0208 <fernando01ferreira+github@gmail.com>
2015-10-31 15:10:46 -0200

    Mecanica de Bang com gesture detecture

---
+++ MovileQuickBuck/Assets/Scripts/GameArcadeManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class GameArcadeManager : MonoBehaviour, TouchObserver {
+
+
+	private bool _playerUpTouched = false;
+	private bool _playerDownTouched = false;
+
+	private GestureDetectureBang _gestureBang;
+
+
+	// Use this for initialization
+	void Start () {
+
+		_gestureBang = GestureDetectureBang.GetSharedGestureDetector();
+
+		_gestureBang.AddListener (this);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnTouchPlayerUp(){
+
+		if (_playerUpTouched == false){
+
+			_playerUpTouched = true;
+
+			if(_playerDownTouched == false) {
+				Debug.Log("Winner PlayerUp");
+
+			}
+		}
+
+	}
+
+	public void OnTouchPlayerDown(){
+
+		if (_playerDownTouched == false){
+			
+			_playerDownTouched = true;
+			
+			if(_playerUpTouched == false) {
+				Debug.Log("Winner PlayerDown");
+
+			}
+		}
+	}
+
+	public void ResetRound (){
+
+		Debug.Log("Reset");
+
+		_playerUpTouched = false;
+		_playerDownTouched = false;
+
+	}
+
+	 
+}
---
+++ MovileQuickBuck/Assets/Scripts/GestureDetectureBang.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public interface TouchObserver {
+	
+	void OnTouchPlayerUp ();
+	void OnTouchPlayerDown ();
+}
+
+public class GestureDetectureBang : MonoBehaviour {
+
+
+
+	///Instancia do Singleton
+	private static GestureDetectureBang sharedInstance;
+
+
+	///Lista de Ouvintes desta classe, que receberam informações quando ocorrer um determinado toque na tela
+	private List<TouchObserver> messageTargets = null;
+
+
+	void Awake (){
+		
+		///Inicialização do Singleton
+		if (sharedInstance == null) {
+			
+			sharedInstance = this;
+			messageTargets = new List<TouchObserver>();
+			
+		} else {
+			Destroy(gameObject);
+		}
+	}
+
+	void Update () 
+	{
+
+		///Verifico a quantidade de toques na tela
+		int nbTouches = Input.touchCount;
+		
+		///Se for maior q 0, verifico o que será feito com cada um desses toques
+		if(nbTouches > 0)
+		{
+			///Para cada toque
+			for (int i = 0; i < nbTouches; i++)
+			{
+				///Pego o toque de posição i no Array de toques
+				Touch touch = Input.GetTouch(i);
+				
+				///Verifico qual é a fase do toque
+				TouchPhase phase = touch.phase;
+				
+				switch(phase)
+				{
+				case TouchPhase.Began:
+
+					if(touch.position.y > Screen.height/2){
+
+						SendMessageToTargets (true);
+
+					}else {
+
+						SendMessageToTargets (false);
+					
+					}
+
+					break;
+				}
+			}
+		}
+	}
+
+
+	///Envio mensagem para os meus Listeners
+	private void SendMessageToTargets(bool isPlayerUp) {
+
+		foreach(TouchObserver go in messageTargets){
+			if(isPlayerUp){
+				go.OnTouchPlayerUp();
+			}else{
+				go.OnTouchPlayerDown();
+			}
+		}
+	}
+
+	///Devolvo a instância deste Singleton
+	public static GestureDetectureBang GetSharedGestureDetector (){
+		return sharedInstance;
+	}
+
+	///Adiciono um Ouvinte na minha lista
+	public void AddListener(TouchObserver go){
+		messageTargets.Add (go);
+	}
+	
+	///Removo um Ouvinte especifico da minha lista
+	public void RemoveListener(TouchObserver go){
+		messageTargets.Remove(go);
+	}
+}
