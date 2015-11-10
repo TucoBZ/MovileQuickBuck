ae8e06f97a0f8faa367acb2232faf323c41b0651
Túlio Bazan da Silva <tucobz@gmail.com>
2015-11-10 08:43:51 -0200

    Splash Screen

--- MovileQuickBuck/Assets/Scripts/MenuPrincipal.cs
+++ MovileQuickBuck/Assets/Scripts/MenuPrincipal.cs
@@ -5,6 +5,12 @@ public class MenuPrincipal : MonoBehaviour {
 
 	private GameController controller;
 
+	void Awake(){
+
+		LoadSplashScreen ();
+
+	}
+
 	void Start(){
 
 		//Ajuste do Controller
@@ -14,6 +20,14 @@ public class MenuPrincipal : MonoBehaviour {
 		controller.versusBT = null;
 		controller.ResetGame ();
 
+	}
+
+	///Abre Alert de compra de AD, senão tiver Internet Abre alert de Conexão
+	private void LoadSplashScreen(){
+		
+		GameObject req = Resources.Load<GameObject> ("Prefabs/SplashScreen_Canvas");
+		Instantiate (req as GameObject, Vector3.zero, Quaternion.identity);
+		
 	}
 
 	public void ArcadeMode(){
---
+++ MovileQuickBuck/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SplashScreen : MonoBehaviour {
+	
+	void Start () {
+		StartCoroutine (WaitToDestroy (2.5f));
+	}
+	
+	IEnumerator WaitToDestroy(float time) {
+		yield return new WaitForSeconds(time);
+		Destroy (gameObject);	
+	}
+
+}
